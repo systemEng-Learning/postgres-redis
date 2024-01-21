c1f28be18fbc20466c44e6387f30e585f2ef2105
Goodness Duru <goodyduru@gmail.com>
2024-01-21 11:42:34 +0100

    Update select query function for hook

--- src/select.rs
+++ src/select.rs
@@ -1,6 +1,16 @@
 use pgrx::prelude::*;
+use std::ffi::CStr;
 
 pub fn handle_select(query_desc: &PgBox<pg_sys::QueryDesc>) {
-    let s = format!("PostgresRedis > Hello Select statement with operation number {}", query_desc.operation);
-    ereport!(PgLogLevel::NOTICE, PgSqlErrorCode::ERRCODE_SUCCESSFUL_COMPLETION, s)
+    let raw_query_string = unsafe { CStr::from_ptr(query_desc.sourceText) };
+    let query_string = raw_query_string
+        .to_str()
+        .expect("Failed to convert Postgres query string for rust");
+
+    let s = format!("PostgresRedis > Hello World!!! Here's your select query {query_string}");
+    ereport!(
+        PgLogLevel::NOTICE,
+        PgSqlErrorCode::ERRCODE_SUCCESSFUL_COMPLETION,
+        s
+    )
 }
