fe7c03c2555d50a28b5025307b363080c00a4da8
Goodness Duru <goodyduru@gmail.com>
2024-01-21 17:27:01 +0100

    Add new table to sql file

--- sql/test.sql
+++ sql/test.sql
@@ -6,3 +6,12 @@ CREATE TABLE test (
 );
 INSERT INTO test (title, description, payload) VALUES ('Fox', 'a description', '{"key": "value"}');
 INSERT INTO test (title, description, payload) VALUES ('Hox', 'a mispelling', '{"select": "update"}');
+
+CREATE TABLE users (
+    id serial8 NOT NULL PRIMARY KEY,
+    first_name varchar(50),
+    last_name varchar(50),
+    password varchar(100)
+);
+
+INSERT INTO users (first_name, last_name, password) VALUES ('Adebayo', 'Chukwuma', '123456'), ('Usman', 'Efe', '987654');
